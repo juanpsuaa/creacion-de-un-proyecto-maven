31fd6e9537ff21267b56f99927f654f077bdfd1e
juanpsuaa <juanpsuaa@gmail.com>
2026-09-22 22:58:25 +0100

    Add files via upload

---
+++ gestor-tareas/src/main/java/com/codelearn/tareas/Main.java
@@ -0,0 +1,11 @@
+package com.codelearn.tareas;
+
+import com.google.gson.Gson;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        var datos = Map.of("titulo", "Aprender Maven", "completada", false);
+        System.out.println(new Gson().toJson(datos));
+    }
+}
